a7605491476bc972db0c6d111cc09fb425ad746e
FusionSid <siddheshadsv@icloud.com>
2025-11-30 02:27:58 +1300

    feat: add timers to run in the background, 1s, 5s & 10s

---
+++ firmware/gps/components/sm/include/timers.h
@@ -0,0 +1,13 @@
+#ifndef SM_TIMERS_H
+#define SM_TIMERS_H
+
+void timers_start_1s(void);
+void timers_stop_1s(void);
+void timers_start_5s(void);
+void timers_stop_5s(void);
+void timers_start_10s(void);
+void timers_stop_10s(void);
+
+void timers_init(void);
+
+#endif
