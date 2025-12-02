f247918f8767bf4730a3721442e4b53f523f9e23
0aids <aidanlldanu@gmail.com>
2025-12-03 12:30:41 +1300

    feat: finalised radio.h-less radio implementations and psat<->ground station fsm

--- firmware/lora/lora-board/src/ground_lora.c
+++ firmware/lora/lora-board/src/ground_lora.c
@@ -1,13 +1,65 @@
 #include "ground_lora.h"
 #include <string.h>
+#include "gps_state.h"
 #include <stdio.h>
 #include "delay.h"
 #include "global_radio.h"
 #include "packets/packets.h"
 #include "global_lora.h"
 #include "radio.h"
+#include "tremo_cm4.h"
 #include "tremo_delay.h"
 #include "timer.h"
+#include "tremo_uart.h"
+
+static gps_state_t g_gpsBuffer;
+
+void PrintGpsState(const gps_state_t *gps)
+{
+    if (!gps) {
+        printf("GPS State: NULL pointer\r\n");
+        return;
+    }
+
+    printf("=== GPS STATE ===\n\r");
+
+    printf("Position:\n\r");
+    printf("  Latitude:        %.6f\r\n", gps->latitude);
+    printf("  Longitude:       %.6f\r\n", gps->longitude);
+    printf("  Altitude:        %.2f m\r\n", gps->altitude);
+    printf("  Geoidal Sep:     %.2f m\r\n", gps->geoidal_sep);
+
+    printf("\r\nSpeed & Course:\r\n");
+    printf("  Speed (knots):   %.2f\r\n", gps->speed_knots);
+    printf("  Speed (kph):     %.2f\r\n", gps->speed_kph);
+    printf("  Course:          %.2f deg\r\n", gps->course_deg);
+
+    printf("\r\nFix / Satellite Info:\r\n");
+    printf("  HDOP:            %.2f\r\n", gps->hdop);
+    printf("  Fix Quality:     %d\r\n", gps->fix_quality);
+    printf("  Sats Tracked:    %d\r\n", gps->satellites_tracked);
+    printf("  Sats in View:    %d\r\n", gps->sats_in_view);
+
+    printf("\r\nTimestamp:\r\n");
+    // BUG: Buggy printf implementation means multiple format specifiers causes a crash!
+    // printf("  Date:            %02d-%02d-%04d\n", gps->day, gps->month, gps->year);
+    // printf("  Time:            %02d:%02d:%02d\n", gps->hours, gps->minutes, gps->seconds);
+    printf("  Date:            %02d", gps->day);
+    printf("-%02d", gps->month);
+    printf("-%04d\r\n", gps->year);
+    printf("  Time:            %02d:", gps->hours);
+    printf("%02d:", gps->minutes);
+    printf("%02d\r\n", gps->seconds);
+
+    printf("\r\nValidity Flags:\r\n");
+    printf("  Position Valid:  %s\r\n", gps->position_valid ? "true" : "false");
+    printf("  Nav Valid:       %s\r\n", gps->nav_valid ? "true" : "false");
+    printf("  Fix Info Valid:  %s\r\n", gps->fix_info_valid ? "true" : "false");
+    printf("  Altitude Valid:  %s\r\n", gps->altitude_valid ? "true" : "false");
+
+    printf("===================\r\n");
+}
+
 
 #define RX_TIMEOUT 0 //ms
 
@@ -27,10 +79,16 @@ void GroundOnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
     // gr_RadioSleep( );
     g_packetRecv = ParsePacket(payload, size);
     printPacketStats(&g_packetRecv);
-    delay_ms(500);
+    delay_ms(50);
     switch (g_packetRecv.type) {
         case PING:
             d_pingResponse();
+            break;
+        case GPS_DATA:
+            printf("Success, received GPS_DATA!\r\n");
+            printf("Payload size: %u\r\n", size);
+            PrintGpsState((gps_state_t*)&g_packetRecv.data);
+            break;
         default:
             break;
     }
@@ -54,16 +112,6 @@ void GroundOnRxError( void )
     // gr_RadioSleep( );
 }
 
-static inline void BlockingRadioSend(uint8_t* buffer, uint16_t bufferSize) {
-    printf("Sending packet!\r\n");
-    gr_RadioSend(buffer, bufferSize);
-    // while (gr_RadioGetStatus() != gr_RadioStates_Idle)
-    // {
-        // Sleep until we finished transmitting.
-    DelayMs(100);
-    // }
-}
-
 void GroundRadioInit(void) {
     gr_RadioSetTxDoneCallback(GroundOnTxDone);
     gr_RadioSetRxDoneCallback(GroundOnRxDone);
@@ -75,36 +123,156 @@ void GroundRadioInit(void) {
     // As a test we'll ping on startup and then listen for the beacon details later afterwards.
     printf("Starting up\r\n");
 
-    printf("Pinging!\r\n");
-    g_packetSend = CreatePacket(PING, NULL, 0);
-    BlockingRadioSend((uint8_t*)&g_packetSend, 1);
 
-    printf("Waiting 2s for response!\r\n");
-    g_timer = TimerGetCurrentTime();
-    while (TimerGetCurrentTime() < g_timer + 5000)
-    {
-        gr_RadioSetRx(0);
-        gr_RadioCheckRecv();
-        DelayMs(10);
-        if (g_packetRecv.type == PONG) {
-            printf("success! Received pong!\r\n");
-            goto pong_success;
-        }
-    }
-    printf("Fail! Pong not received \r\n");
-
-pong_success:
     printf("Initial communication test complete\r\n");
     printf("Falling back to receive only mode (Pingpong only)\r\n");
     gr_RadioSetRx(0);
 }
 
+#define d_uartInputBufferSize 256
+char g_uartInputBuffer[d_uartInputBufferSize] = {0};
+
+#define d_possibleInstXMacro \
+    X(gpsReq) \
+    X(foldReq) \
+    X(buzReq) \
+    X(ping) \
+
+#define X(name) possibleInst_##name,
+typedef enum {
+    d_possibleInstXMacro
+} e_possibleInst;
+
+#undef X
+
+#define X(name) #name,
+
+const char * const g_possibleInst[] = {
+    d_possibleInstXMacro
+};
+
+#undef X
+
 uint8_t g_groundRadioMainIters = 0;
 void GroundRadioMain() {
-    // For now, set the PSAT to be a beacon broadcasting some random ass text.
     g_groundRadioMainIters++;
     e_radioState radioState = gr_RadioGetStatus();
-    if (g_groundRadioMainIters % 20)
+    if (g_groundRadioMainIters % 50 == 0)
         printf("State: %s\r\n", gr_RadioStatesToString(radioState));
     gr_RadioCheckRecv();
+    // Check for any instructions from uart
+
+    if (uart_get_flag_status(CONFIG_DEBUG_UART, UART_FLAG_RX_FIFO_EMPTY) == RESET)
+    {
+        // Clear the buffer
+        uint16_t i = 0;
+        while(uart_get_flag_status(CONFIG_DEBUG_UART, UART_FLAG_RX_FIFO_EMPTY) == RESET &&
+              i < d_uartInputBufferSize - 1)
+        {
+            g_uartInputBuffer[i++] = uart_receive_data(CONFIG_DEBUG_UART);
+            // DelayMs(10);
+        }
+        g_uartInputBuffer[i] = 0;
+        printf("Received uart input: \"%s\"\r\n", g_uartInputBuffer);
+
+        if (strcmp(g_uartInputBuffer, g_possibleInst[possibleInst_buzReq]) == 0) 
+        {
+            printf("Requesting buzzer!\r\n");
+            g_packetSend = CreatePacket(BUZ_REQ, NULL, 0);
+            gr_RadioSend((uint8_t*)&g_packetSend, 1);
+            // This is required otherwise we will get a tx timeout.
+            DelayMs(100) ;
+
+            printf("Waiting 2s for response!\r\n");
+            g_timer = TimerGetCurrentTime();
+            while (TimerGetCurrentTime() < g_timer + 2000)
+            {
+                gr_RadioSetRx(0);
+                gr_RadioCheckRecv();
+                DelayMs(10);
+                if (g_packetRecv.type == ACK) {
+                    printf("Success, received ack!\r\n");
+                    goto buz_req_success;
+                }
+            }
+            printf("Fail! ACK not received \r\n");
+        buz_req_success:
+            ;
+        }
+        else if (strcmp(g_uartInputBuffer, g_possibleInst[possibleInst_foldReq]) == 0) 
+        {
+            printf("Requesting fold!\r\n");
+            g_packetSend = CreatePacket(FOLD_REQ, NULL, 0);
+            gr_RadioSend((uint8_t*)&g_packetSend, 1);
+            // This is required otherwise we will get a tx timeout.
+            DelayMs(100) ;
+
+            printf("Waiting 2s for response!\r\n");
+            g_timer = TimerGetCurrentTime();
+            while (TimerGetCurrentTime() < g_timer + 2000)
+            {
+                gr_RadioSetRx(0);
+                gr_RadioCheckRecv();
+                DelayMs(10);
+                if (g_packetRecv.type == ACK) {
+                    printf("Success, received ACK!\r\n");
+                    goto fold_req_success;
+                }
+            }
+            printf("Fail! ACK not received \r\n");
+        fold_req_success:
+            ;
+        }
+        else if (strcmp(g_uartInputBuffer, g_possibleInst[possibleInst_ping]) == 0) 
+        {
+            printf("Pinging!\r\n");
+            g_packetSend = CreatePacket(PING, NULL, 0);
+            gr_RadioSend((uint8_t*)&g_packetSend, 1);
+            // This is required otherwise we will get a tx timeout.
+            DelayMs(100) ;
+
+            printf("Waiting 2s for response!\r\n");
+            g_timer = TimerGetCurrentTime();
+            while (TimerGetCurrentTime() < g_timer + 2000)
+            {
+                gr_RadioSetRx(0);
+                gr_RadioCheckRecv();
+                DelayMs(10);
+                if (g_packetRecv.type == PONG) {
+                    printf("success! Received pong!\r\n");
+                    goto pong_success;
+                }
+            }
+            printf("Fail! Pong not received \r\n");
+        pong_success:
+            ;
+        }
+        else if (strcmp(g_uartInputBuffer, g_possibleInst[possibleInst_gpsReq]) == 0) 
+        {
+            printf("Requesting GPS!\r\n");
+            g_packetSend = CreatePacket(GPS_REQ, NULL, 0);
+            gr_RadioSend((uint8_t*)&g_packetSend, g_packetSend.m_dataSize + 1);
+            // This is required otherwise we will get a tx timeout.
+            DelayMs(100);
+
+            printf("Waiting 2s for response!\r\n");
+            g_timer = TimerGetCurrentTime();
+            while (TimerGetCurrentTime() < g_timer + 2000)
+            {
+                gr_RadioSetRx(0);
+                gr_RadioCheckRecv();
+                DelayMs(10);
+                if (g_packetRecv.type == GPS_DATA) {
+                    printf("Success, received GPS_DATA!\r\n");
+                    memcpy(&g_gpsBuffer, g_packetRecv.data, g_packetRecv.m_dataSize);
+                    PrintGpsState(&g_gpsBuffer);
+                    goto gps_data_success;
+                }
+            }
+            printf("FAIL, no gps data was received\r\n");
+        gps_data_success:
+            // And then we need to ensure we also get the data.
+            ;
+        }
+    }
 }
