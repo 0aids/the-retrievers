a03ede2546dd45152decd592e7fa135b67961b7b
TFEPhoenix <phoenixma3101@gmail.com>
2025-12-03 16:11:39 +1300

    test: made the test code simpler so it works

--- firmware/servo/main/main.c
+++ firmware/servo/main/main.c
@@ -9,44 +9,30 @@
 
 #include "driver/gpio.h"
 
-#define interrupt_pin           (25)
+#define button           (25)
 
-void loop(servo_t *servo_state, bool deploy){
-    // Changes the angle if the PSAT is deployed. Remains at 0 otherwise.
-    if (deploy){
-        servo_set_angle(servo_state , 60);
-    }
-    else{
-        servo_set_angle(servo_state, 0);
-    }
-}
 
 // For testing
-static void IRAM_ATTR gpio_isr_handler(void *arg){
-    gpio_isr_handler_add(interrupt_pin, gpio_isr_handler, NULL);
-    gpio_intr_enable(interrupt_pin);
-}
+void app_main(void) { 
+    gpio_reset_pin(button);
 
-
-void app_main(void) {
-    // For testing
-    gpio_reset_pin(interrupt_pin);
-    gpio_set_direction(interrupt_pin, GPIO_MODE_INPUT);
-
-    gpio_set_pull_mode(interrupt_pin, GPIO_PULLUP_ONLY);
-
-    gpio_set_intr_type(interrupt_pin, GPIO_INTR_POSEDGE);
-
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(interrupt_pin, gpio_isr_handler, NULL);
-
-    gpio_intr_enable(interrupt_pin);
+    gpio_set_pull_mode(button, GPIO_PULLUP_ONLY);
+    gpio_set_direction(button, GPIO_MODE_INPUT);
 
     // Sets up the servo
     servo_t servo_state = servo_setup();
     // Loops to check if deployed
-    bool deploy = false;
     while (1){
-        loop(&servo_state, deploy);
+        int status = gpio_get_level(button);
+        if (status){
+            servo_set_angle(&servo_state, 0);
+            printf("Angle at 0\n");
+        }
+        else{
+
+            servo_set_angle(&servo_state , 60);
+            printf("Angle at 60\n");
+        }
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
