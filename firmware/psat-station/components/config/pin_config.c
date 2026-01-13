269a5adfb272606f96079e77dc948aeaa171b6f5
FusionSid <siddheshadsv@icloud.com>
2026-01-14 12:39:18 +1300

    chore: add ldr and buzzer pins to config

--- firmware/psat-station/components/config/pin_config.c
+++ firmware/psat-station/components/config/pin_config.c
@@ -1,10 +1,14 @@
 #include "pin_config.h"
 
-static const config_pinConfig_t board_cfg = {.gpsUartNum = 1,
-                                             .gpsTxPin = 27,
-                                             .gpsRxPin = 25,
-                                             .landingButtonPin = 19,
-                                             .prelaunchButtonPin = 21,
-                                             .ldrPin = 22};
+static const config_pinConfig_t board_cfg = {
+    .gpsUartNum = UART_NUM_1,
+    .gpsTxPin = 27,
+    .gpsRxPin = 25,
+    .landingButtonPin = 19,
+    .prelaunchButtonPin = 21,
+    .light1Pin = 22,
+    .light2Channel = ADC2_CHANNEL_0,
+    .servoPin = 13,
+};
 
 const config_pinConfig_t* config_getPinConfig(void) { return &board_cfg; }
