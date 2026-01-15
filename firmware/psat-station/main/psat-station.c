947f498e4c9e7565bf3e36f99edd198b309e7cb1
TFEPhoenix <phoenixma3101@gmail.com>
2026-01-15 18:01:35 +1300

    feat: changed sidd's code to use my functions

--- firmware/psat-station/main/psat-station.c
+++ firmware/psat-station/main/psat-station.c
@@ -6,24 +6,5 @@
 #include "ldr.h"
 
 void app_main(void) {
-    // psatFSM_start();
-    // psatFSM_startAsTask();  // running it in task so i can put other code in
-    // this file, however it wont be a task in prod
-    ESP_LOGI(TAG, "Hello world");
-    ldr_setup();
-    ldr_state state1 = ldr_queryState();
-    ESP_LOGI(TAG, "%s",state1.stateString);
-
-    ESP_LOGI(TAG, "voltage: %dmV", ldr_getVoltage());        
-    vTaskDelay(1000);
-
-    ESP_LOGI(TAG, "voltage: %dmV", ldr_getVoltage());        
-    vTaskDelay(1000);
-
-    ESP_LOGI(TAG, "voltage: %dmV", ldr_getVoltage());        
-    vTaskDelay(1000);
-
-    ldr_deinit();
-    ldr_state state3 = ldr_queryState();
-    ESP_LOGI(TAG, "%s",state3.stateString);
+    psatFSM_start();
 }
