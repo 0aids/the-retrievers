9a1b21b8bdecfee795ceb771cc678aa2112dba85
FusionSid <siddheshadsv@icloud.com>
2026-01-19 00:29:33 +1300

    test: add many more tests for the buzzer

--- firmware/psat-station/components/buzzers/test/test_audio.c
+++ firmware/psat-station/components/buzzers/test/test_audio.c
@@ -7,10 +7,35 @@
 
 #define LOW 0
 #define HIGH 1
+#define REACTION_TIME \
+    50  // how much delay to wait before checking pins for update
 
 static const char* TAG = "Buzzers-Test";
 
-TEST_CASE("Buzzer GPIO pin goes HIGH on beep", "[audio]") {
+TEST_CASE("Buzzer init sets GPIO LOW", "[audio]") {
+    const int pin = config_getPinConfig()->buzzerPin;
+
+    buzzer_init();
+    gpio_set_direction(pin, GPIO_MODE_INPUT_OUTPUT);
+
+    TEST_ASSERT_EQUAL(LOW, gpio_get_level(pin));
+}
+
+TEST_CASE("Buzzer beep with zero duration does nothing", "[audio]") {
+    const int pin = config_getPinConfig()->buzzerPin;
+
+    buzzer_init();
+    gpio_set_direction(pin, GPIO_MODE_INPUT_OUTPUT);
+
+    gpio_set_level(pin,
+                   LOW);  // test 1 will have ensured this, but just in case
+    buzzer_beep(0);
+
+    vTaskDelay(pdMS_TO_TICKS(REACTION_TIME));
+    TEST_ASSERT_EQUAL(LOW, gpio_get_level(pin));
+}
+
+TEST_CASE("Buzzer GPIO pin goes LOW HIGH LOW on beep", "[audio]") {
     const int pin = config_getPinConfig()->buzzerPin;
     const int duration_ms = 2500;
 
@@ -20,16 +45,64 @@ TEST_CASE("Buzzer GPIO pin goes HIGH on beep", "[audio]") {
 
     int before, during, after;
     before = gpio_get_level(pin);
+
     buzzer_beep(duration_ms);
-
     during = gpio_get_level(pin);
-    vTaskDelay(pdMS_TO_TICKS(duration_ms +
-                             100));  // give it about 100ms extra to go LOW
 
+    vTaskDelay(pdMS_TO_TICKS(
+        duration_ms + REACTION_TIME));  // give it about 100ms extra to go LOW
     after = gpio_get_level(pin);
-    ESP_LOGI(TAG, "GPIO LEVELS: %i, %i, %i", before, during, after);
 
     TEST_ASSERT_EQUAL(LOW, before);
     TEST_ASSERT_EQUAL(HIGH, during);
     TEST_ASSERT_EQUAL(LOW, after);
+}
+
+TEST_CASE("buzzer turn off stops active beep", "[audio]") {
+    const int pin = config_getPinConfig()->buzzerPin;
+
+    buzzer_init();
+    gpio_set_direction(pin, GPIO_MODE_INPUT_OUTPUT);
+
+    buzzer_beep(500);
+    vTaskDelay(pdMS_TO_TICKS(50));
+    buzzer_turnOff();  // should cut the 500ms beep short
+
+    vTaskDelay(pdMS_TO_TICKS(REACTION_TIME));
+    TEST_ASSERT_EQUAL(LOW, gpio_get_level(pin));
+}
+
+TEST_CASE("Buzzer beep mid another beep uses most recent beep (extending)",
+          "[audio]") {
+    const int pin = config_getPinConfig()->buzzerPin;
+
+    buzzer_init();
+    gpio_set_direction(pin, GPIO_MODE_INPUT_OUTPUT);
+
+    buzzer_beep(500);  // original beep is 500ms but we will extend it
+    vTaskDelay(pdMS_TO_TICKS(250));
+
+    buzzer_beep(2500);
+    vTaskDelay(pdMS_TO_TICKS(REACTION_TIME));
+    TEST_ASSERT_EQUAL(HIGH, gpio_get_level(pin));
+
+    vTaskDelay(pdMS_TO_TICKS(2500));
+    TEST_ASSERT_EQUAL(LOW, gpio_get_level(pin));
+}
+
+TEST_CASE("Buzzer beep mid another beep uses most recent beep (reducing)",
+          "[audio]") {
+    const int pin = config_getPinConfig()->buzzerPin;
+
+    buzzer_init();
+    gpio_set_direction(pin, GPIO_MODE_INPUT_OUTPUT);
+
+    buzzer_beep(2500);  // original beep is 500ms but we will this time lower it
+    vTaskDelay(pdMS_TO_TICKS(250));
+
+    buzzer_beep(500);
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    vTaskDelay(pdMS_TO_TICKS(REACTION_TIME));
+    TEST_ASSERT_EQUAL(LOW, gpio_get_level(pin));
 }
