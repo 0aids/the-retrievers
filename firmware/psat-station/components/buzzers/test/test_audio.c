65808630aa31e964feb420b9d844c4f02b9313fd
FusionSid <siddheshadsv@icloud.com>
2026-01-19 00:05:53 +1300

    test: buzzer gpio pin goes HIGH during beep

--- firmware/psat-station/components/buzzers/test/test_audio.c
+++ firmware/psat-station/components/buzzers/test/test_audio.c
@@ -1,9 +1,35 @@
 #include "../include/buzzer.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "pin_config.h"
 #include "unity.h"
 
-TEST_CASE("checks if the beep works", "[audio]") {
+#define LOW 0
+#define HIGH 1
+
+static const char* TAG = "Buzzers-Test";
+
+TEST_CASE("Buzzer GPIO pin goes HIGH on beep", "[audio]") {
+    const int pin = config_getPinConfig()->buzzerPin;
+    const int duration_ms = 2500;
+
     buzzer_init();
-    buzzer_beep(2500);
-    // not really a proper unit test as no assertion and its confirmed by human
-    // verification
+    gpio_set_direction(
+        pin, GPIO_MODE_INPUT_OUTPUT);  // overwrite default which is OUTPUT only
+
+    int before, during, after;
+    before = gpio_get_level(pin);
+    buzzer_beep(duration_ms);
+
+    during = gpio_get_level(pin);
+    vTaskDelay(pdMS_TO_TICKS(duration_ms +
+                             100));  // give it about 100ms extra to go LOW
+
+    after = gpio_get_level(pin);
+    ESP_LOGI(TAG, "GPIO LEVELS: %i, %i, %i", before, during, after);
+
+    TEST_ASSERT_EQUAL(LOW, before);
+    TEST_ASSERT_EQUAL(HIGH, during);
+    TEST_ASSERT_EQUAL(LOW, after);
 }
