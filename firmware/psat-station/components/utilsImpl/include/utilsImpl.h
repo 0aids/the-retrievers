3f10e6d27ba98cc15f4e2b793d2a688d3ac1ed13
0aids <aidanlldanu@gmail.com>
2026-01-19 14:37:56 +1300

    fix: change utilsImpl.h to be a symlink instead of a file

--- firmware/psat-station/components/utilsImpl/include/utilsImpl.h
+++ firmware/psat-station/components/utilsImpl/include/utilsImpl.h
@@ -1,13 +1,1 @@
-#ifndef shared_utils_h_INCLUDED
-#define shared_utils_h_INCLUDED
-// Anything that is required by a shared logic
-// This is mainly sleeping functions and logging functions
-
-// Defined in the utils.c of your implementation.
-#include <stdint.h>
-#include <stdarg.h>
-extern void utils_sleepMs(uint16_t milliseconds);
-
-extern void utils_log(const char* format, ...);
-
-#endif // shared_utils_h_INCLUDED
+../../../../platform-agnostic/required-impls/utilsImpl.h
