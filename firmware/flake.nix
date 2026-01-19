1e878959793b43661d39411ca2e15ac5d74cb33a
FusionSid <siddheshadsv@icloud.com>
2026-01-19 15:38:40 +1300

    fix: precommit to work

--- firmware/flake.nix
+++ firmware/flake.nix
@@ -9,22 +9,31 @@
 
   outputs = { self, nixpkgs, esp-dev }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      # Choose your target: esp32-idf, esp32s3-idf, esp8266-rtos-sdk, etc.
-      esp-shell = esp-dev.devShells.${system}.esp-idf-full;
+      # Define the architectures you want to support
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset for each system
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      devShells.${system}.default = esp-shell.overrideAttrs (
-          oldShell: {
-              nativeBuildInputs = (oldShell.nativeBuildInputs or []) ++ [
-                  # pkgs.clang-tools
-                  # pkgs.gnumake
-                  # pkgs.cmake
-                  # pkgs.bear
-              ];
-          }
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          # Access the specific shell for the current system
+          esp-shell = esp-dev.devShells.${system}.esp-idf-full;
+        in
+        {
+          default = esp-shell.overrideAttrs (oldShell: {
+            nativeBuildInputs = (oldShell.nativeBuildInputs or [ ]) ++ [
+              # Add extra packages here if needed:
+              # pkgs.cmake
+              # pkgs.ninja
+            ];
+            # shellHook = (oldShell.shellHook or "") + ''
+            #   export IDF_TOOLCHAIN=clang
+            # '';
+          });
+        }
       );
-    }
-    ;
+    };
 }
