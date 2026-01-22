d430ba8a5f4a802e0b86855ec0b3ffa0cb2df435
TFEPhoenix <phoenixma3101@gmail.com>
2026-01-23 12:40:59 +1300

    refactor: moved configuration of ldr and renamed unclear variables

--- firmware/psat-station/components/config/include/pin_config.h
+++ firmware/psat-station/components/config/include/pin_config.h
@@ -2,14 +2,15 @@
 
 #include "driver/uart.h"
 
+
 #define CFG_GPS_UART_NUM_d         UART_NUM_1
-#define CFG_GPS_TX_PIN_d           27
-#define CFG_GPS_RX_PIN_d           25
-#define CFG_LANDING_BUTTON_PIN_d   22
-#define CFG_PRELAUNCH_BUTTON_PIN_d 21
-#define CFG_LIGHT1_PIN_d           19
-#define CFG_BUZZER_PIN_d           23
-#define CFG_SERVO_PIN_d            13
+// #define CFG_GPS_TX_PIN_d           27
+// #define CFG_GPS_RX_PIN_d           25
+// #define CFG_LANDING_BUTTON_PIN_d   22
+// #define CFG_PRELAUNCH_BUTTON_PIN_d 21
+// #define CFG_LIGHT1_PIN_d           19
+// #define CFG_BUZZER_PIN_d           23
+// #define CFG_SERVO_PIN_d            13
 
 #define CFG_GPS_TX_PIN_d           38
 #define CFG_GPS_RX_PIN_d           37
@@ -18,3 +19,12 @@
 #define CFG_LIGHT1_PIN_d           2
 #define CFG_BUZZER_PIN_d           21
 #define CFG_SERVO_PIN_d            8
+
+#define LDR_ADC_UNIT_d             ADC_UNIT_2
+#define LDR_ADC_CHANNEL_d          ADC_CHANNEL_7
+#define LDR_ULP_MODE_d             ADC_ULP_MODE_DISABLE
+#define LDR_ADC_ATTEN_d            ADC_ATTEN_DB_12
+#define LDR_ADC_BITWIDTH_d         ADC_BITWIDTH_DEFAULT
+#define LDR_PIN_MASK_d             (1ULL << 18)
+#define LDR_STATE_CONFIG_BUFFER_SIZE_d (1024)
+
