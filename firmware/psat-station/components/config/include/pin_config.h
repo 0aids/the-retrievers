90d4248725559c4058551495dd5bd406534252d3
TFEPhoenix <phoenixma3101@gmail.com>
2026-01-23 14:32:43 +1300

    chore: fixed the stupid pin config to match the pcb

--- firmware/psat-station/components/config/include/pin_config.h
+++ firmware/psat-station/components/config/include/pin_config.h
@@ -11,10 +11,23 @@
 #define CFG_BUZZER_PIN_d           23
 #define CFG_SERVO_PIN_d            13
 
-#define CFG_GPS_TX_PIN_d           38
-#define CFG_GPS_RX_PIN_d           37
+#define CFG_GPS_TX_PIN_d           37
+#define CFG_GPS_RX_PIN_d           38 // We don't care about this. 
 #define CFG_LANDING_BUTTON_PIN_d   39
 #define CFG_PRELAUNCH_BUTTON_PIN_d 40
 #define CFG_LIGHT1_PIN_d           2
 #define CFG_BUZZER_PIN_d           21
+<<<<<<< Updated upstream
 #define CFG_SERVO_PIN_d            8
+=======
+#define CFG_SERVO_PIN_d            8
+
+#define LDR_ADC_UNIT_d             ADC_UNIT_1
+#define LDR_ADC_CHANNEL_d          ADC_CHANNEL_1
+#define LDR_ULP_MODE_d             ADC_ULP_MODE_DISABLE
+#define LDR_ADC_ATTEN_d            ADC_ATTEN_DB_12
+#define LDR_ADC_BITWIDTH_d         ADC_BITWIDTH_DEFAULT
+#define LDR_PIN_MASK_d             (1ULL << 18)
+#define LDR_STATE_CONFIG_BUFFER_SIZE_d (1024)
+
+>>>>>>> Stashed changes
