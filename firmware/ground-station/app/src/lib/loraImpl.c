dfc2f89a404349871772853a82aae20264147f26
FusionSid <siddheshadsv@icloud.com>
2026-01-24 14:14:03 +1300

    feat: add loraImpl library for use in C

---
+++ firmware/ground-station/app/src/lib/loraImpl.c
@@ -0,0 +1,57 @@
+#include "netinet/in.h"
+#include "loraImpl.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+// Global State
+loraImpl_globalState_t loraImpl_globalState_g = {
+    .onTxDone               = NULL,
+    .onRxDone               = NULL,
+    .onRxError              = NULL,
+    .onRxTimeout            = NULL,
+    .onTxTimeout            = NULL,
+    .dataBuffer             = {},
+    .dataCurrentContentSize = {},
+    .rssi                   = {},
+    .snr                    = {},
+};
+
+void (*loraImpl_onRxDoneCallback)(uint8_t* payload, uint16_t size,
+                                  int16_t rssi, int8_t snr);
+
+void loraImpl_init(void) {}
+
+void loraImpl_deinit(void) {}
+
+void loraImpl_setCallbacks(void (*onTxDone)(void),
+                           void (*onRxDone)(uint8_t* payload,
+                                            uint16_t size,
+                                            int16_t rssi, int8_t snr),
+                           void (*onTxTimeout)(void),
+                           void (*onRxTimeout)(void),
+                           void (*onRxError)(void))
+{
+    loraImpl_onRxDoneCallback = onRxDone;
+}
+
+void loraImpl_send(uint8_t* payload, uint16_t payloadSize) {}
+
+void loraImpl_irqProcess(void) {}
+
+void loraImpl_setRx(uint32_t milliseconds) {}
+
+void loraImpl_setIdle() {}
+
+void loraImpl_queryState(void)
+{
+    // noop
+}
