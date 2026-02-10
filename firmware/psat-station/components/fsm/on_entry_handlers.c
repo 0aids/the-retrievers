371096ab39bb0d356f9cf960b36da7ee682925bc
FusionSid <siddheshadsv@icloud.com>
2026-02-10 14:38:51 +1300

    chore: woopsie daisies spelling mistake cspell wouldnt havelet this happen

--- firmware/psat-station/components/fsm/on_entry_handlers.c
+++ firmware/psat-station/components/fsm/on_entry_handlers.c
@@ -17,12 +17,16 @@ void loraFSM_startAsTask()
                 NULL);
 }
 
+void test() {
+    printf("hey there my name is test and i was called\n");
+}
+
 void psatFSM_prelaunchEntryHandler()
 {
     gpio_install_isr_service(0);
 
     psatFSM_registerComponent(psatFSM_component_gps, gps_init,
-                              gps_killTask, NULL);
+                              NULL, test);
     psatFSM_registerComponent(psatFSM_component_timers, timer_init,
                               NULL, NULL);
     psatFSM_registerComponent(psatFSM_component_buzzers, buzzer_init,
