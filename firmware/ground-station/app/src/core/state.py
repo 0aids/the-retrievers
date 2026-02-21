90db31899f7514d9ab3c84e43b7b9b17687c3e09
FusionSid <siddheshadsv@icloud.com>
2026-02-21 17:24:39 +1300

    feat: update state to be same as frontend, also fancy decorator

--- firmware/ground-station/app/src/core/state.py
+++ firmware/ground-station/app/src/core/state.py
@@ -1,7 +1,8 @@
+import time
 import ctypes
 import threading
-import time
 from copy import deepcopy
+from functools import wraps
 from enum import IntEnum, auto
 
 
@@ -45,32 +46,66 @@ class GPSStruct(ctypes.Structure):
     ]
 
 
+def with_lock(writing: bool = True):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(self: "StateManager", *args, **kwds):
+            with self._lock:
+                if writing:
+                    self._state["radio"]["lastPacketTime"] = time.time()
+                    self._state["radio"]["packetsReceived"] += 1
+
+                return f(self, *args, **kwds)
+
+        return wrapper
+
+    return decorator
+
+
 class StateManager:
     def __init__(self):
         self._lock = threading.Lock()
+        # I'm using camel case in C and TS so i'm using it here for consistency
+        # im sorry python gods, i love snake case i really do
         self._state = {
-            "gps": {field[0]: None for field in GPSStruct._fields_},
-            "fsm": {"state": None, "name": None},
-            "radio": {"last_packet_time": None},
-            "stats": {"packets_received": 0},
+            "preflightSuccess": None,
+            "data": {
+                "gps": {field[0]: None for field in GPSStruct._fields_},
+                "ldrVoltage": None,
+                "servoAngle": None,
+                "batteryLevel": None,
+                "temperature": None,
+                "cameraOn": None,
+                "pressure": None,
+            },
+            "fsm": {
+                "currentState": None,
+                "currentStateName": None,
+                "prevState": None,
+                "prevStateName": None,
+            },
+            "components": [],
+            "radio": {
+                "lastPacketTime": None,
+                "packetsReceived": 0,
+            },
         }
 
+    @with_lock()
     def update_gps(self, gps: dict):
-        with self._lock:
-            self._state["gps"].update(gps)
-            self._state["radio"]["last_packet_time"] = time.time()
-            self._state["stats"]["packets_received"] += 1
+        self._state["data"]["gps"].update(gps)
 
-    def update_state(self, new_state: int | FSMState):
-        with self._lock:
-            self._state["fsm"]["state"] = FSMState(new_state)
-            self._state["fsm"]["name"] = FSMState(new_state).name
-            self._state["radio"]["last_packet_time"] = time.time()
-            self._state["stats"]["packets_received"] += 1
+    @with_lock()
+    def update_state(self, new_state: int, prev_state: int):
+        self._state["fsm"]["currentState"] = FSMState(new_state)
+        self._state["fsm"]["currentStateName"] = FSMState(new_state).name
 
+        self._state["fsm"]["prevState"] = FSMState(prev_state)
+        self._state["fsm"]["prevStateName"] = FSMState(prev_state).name
+
+    @with_lock(False)
     def snapshot(self):
-        with self._lock:
-            return deepcopy(self._state)
+        return deepcopy(self._state)
 
 
 state_manager = StateManager()
