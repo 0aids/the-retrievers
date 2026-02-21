9d89f3ace7f657de2058bec994fd888792a0d35b
FusionSid <siddheshadsv@icloud.com>
2026-02-21 18:13:47 +1300

    feat: send args too command route

--- firmware/ground-station/app/src/routes/api/command.py
+++ firmware/ground-station/app/src/routes/api/command.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 from core.commands import handle_command
+from lib import PacketType
 
 command_router = APIRouter(
     tags=[
@@ -11,14 +12,14 @@ command_router = APIRouter(
 
 
 class Command(BaseModel):
-    cmd: int
-    args: dict = {}
+    cmd: PacketType
+    args: int | None = None
 
 
 @command_router.post("/api/command")
 def send_command(command: Command):
     try:
-        print("Handle Command...")
+        print(f"Handle Command ({command.cmd.name})...")
         handle_command(command.cmd, command.args)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
--- firmware/ground-station/app/src/routes/api/state.py
+++ firmware/ground-station/app/src/routes/api/state.py
@@ -1,3 +1,20 @@
+# from fastapi import APIRouter
+
+# from core.state import state_manager
+
+# state_router = APIRouter(
+#     tags=[
+#         "State",
+#     ],
+# )
+
+
+# @state_router.get("/api/state")
+# def get_state():
+#     return state_manager.snapshot()
+
+
+import random
 from fastapi import APIRouter
 
 from core.state import state_manager
@@ -8,7 +25,49 @@ state_router = APIRouter(
     ],
 )
 
+counter = 0
+lat: float | None = None
+long: float | None = None
+
+
+def random_coords():
+    global lat, long, counter
+
+    counter += 1
+
+    if counter > 20:
+        lat = -36.852646
+        long = 174.770197
+
+    if counter > 100:
+        return
+
+    if lat and long:
+        lat += random.uniform(-0.0001, 0.0001)
+        long += random.uniform(-0.0001, 0.0001)
+
 
 @state_router.get("/api/state")
 def get_state():
-    return state_manager.snapshot()
+    random_coords()
+    s = state_manager.snapshot()
+    if counter > 10:
+        state_manager.update_state(2, 1)
+    else:
+        state_manager.update_state(1, 0)
+
+    s["data"]["gps"]["positionValid"] = counter > 10
+    s["data"]["gps"]["latitude"] = lat
+    s["data"]["gps"]["longitude"] = long
+
+    s["components"] = [
+        {"name": "Battery", "status": True},
+        {"name": "LDR", "status": True},
+        {"name": "Servo", "status": True},
+        {"name": "Timers", "status": True},
+        {"name": "GPS", "status": True},
+        {"name": "Pressure", "status": False},
+        {"name": "Camera", "status": True},
+        {"name": "Buzzers", "status": True},
+    ]
+    return s
