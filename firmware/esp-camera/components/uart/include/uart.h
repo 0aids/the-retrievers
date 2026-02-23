e675cd0542b1817cff06fde2369b0c278a5ed95c
TFEPhoenix <phoenixma3101@gmail.com>
2026-02-23 17:31:51 +1300

    refactor: cleaned up main.c and moved uart functions to uart component

--- firmware/esp-camera/components/uart/include/uart.h
+++ firmware/esp-camera/components/uart/include/uart.h
@@ -6,9 +6,13 @@
 #include "driver/uart.h"
 #include "esp_log.h"
 
+
 #define CAM_UART_NUM        UART_NUM_0
 #define UART_TX             (1)
 #define UART_RX             (3)
 #define BUF_SIZE            (1024*2)
 #define STACK_SIZE          (3072)
+#define UART_BAUD_RATE      (115200)
 
+void uart_init(void);
+void uart_task(void *arg);
--- firmware/esp-camera/components/uart/uart.c
+++ firmware/esp-camera/components/uart/uart.c
@@ -1,11 +1,10 @@
 #include "uart.h"
 
-void uart_task(void *arg){
+void uart_init(void){
     // Install UART driver using an event queue here
     
-    
     uart_config_t uart_config = {
-        .baud_rate = 115200,
+        .baud_rate = UART_BAUD_RATE,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
@@ -16,17 +15,27 @@ void uart_task(void *arg){
     ESP_ERROR_CHECK(uart_driver_install(CAM_UART_NUM, BUF_SIZE, 0, 0, NULL, intr_alloc_flags));
     ESP_ERROR_CHECK(uart_param_config(CAM_UART_NUM, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(CAM_UART_NUM, UART_TX, UART_RX, -1, -1));
+}
 
+void uart_task(void *arg){
     // Configure a temporary buffer for the incoming data
     uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+    bool listen;
+    listen = true;
 
-    while (1) {
+    while (listen) {
         // Read data from the UART
         int len = uart_read_bytes(CAM_UART_NUM, data, (BUF_SIZE - 1), 500 / portTICK_PERIOD_MS);
         
         if (len) {
+            data[len] = 0;
             // Write data back to the UART
             uart_write_bytes(CAM_UART_NUM, (const char *) data, len);
+            if(strstr((const char*)data, "DEINIT")){
+                free(data);
+                listen = false;
+                vTaskDelete(NULL);
+            }
         }
     }
 }
