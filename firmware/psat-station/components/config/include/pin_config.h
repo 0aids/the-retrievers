dfd79ce488ed87036cac9f4d0f279cbf96a6e249
FusionSid <siddheshadsv@icloud.com>
2026-02-24 16:52:01 +1300

    feat: update pins for new pcb

--- firmware/psat-station/components/config/include/pin_config.h
+++ firmware/psat-station/components/config/include/pin_config.h
@@ -3,13 +3,13 @@
 #include "driver/uart.h"
 
 #define CFG_GPS_UART_NUM_d         UART_NUM_1
-#define CFG_GPS_TX_PIN_d           48 // 37 // CANNOT USE! Part of heap.
-#define CFG_GPS_RX_PIN_d           42
-#define CFG_LANDING_BUTTON_PIN_d   39
-#define CFG_PRELAUNCH_BUTTON_PIN_d 21
-#define CFG_LIGHT1_PIN_d           41
-#define CFG_BUZZER_PIN_d           40
-#define CFG_SERVO_PIN_d            8
+#define CFG_GPS_TX_PIN_d           6 // 37 // CANNOT USE! Part of heap.
+#define CFG_GPS_RX_PIN_d           7
+#define CFG_LANDING_BUTTON_PIN_d   47
+#define CFG_PRELAUNCH_BUTTON_PIN_d 45
+#define CFG_LIGHT1_PIN_d           42
+#define CFG_BUZZER_PIN_d           38
+#define CFG_SERVO_PIN_d            48
 
 #define LDR_ADC_UNIT_d                 ADC_UNIT_1
 #define LDR_ADC_CHANNEL_d              ADC_CHANNEL_1
