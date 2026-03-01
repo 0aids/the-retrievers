29c7b1cba872d22523f26c8f180632a01797e52c
FusionSid <siddheshadsv@icloud.com>
2026-03-01 23:57:49 +1300

    feat: psat send ACK packet and maintains a sequence number cache ground station does not currently resend packet if it doesnt receive ack also all untested as i dont have the mcu on me rn

--- firmware/ground-station/app/src/core/commands.py
+++ firmware/ground-station/app/src/core/commands.py
@@ -1,14 +1,19 @@
-from lib import lora_send
+import itertools
 
 from rich import print
 from rich.panel import Panel
 
+from lib import lora_send
+
+sequence_number = itertools.count()
+
 
 def handle_command(cmd: int, args: int | None = None):
-    data = cmd.to_bytes()
+    data = cmd.to_bytes(byteorder="little")
+    data += next(sequence_number).to_bytes(length=2, byteorder="little")
 
     if args is not None:
-        data += args.to_bytes()
+        data += args.to_bytes(byteorder="little")
 
     print(
         Panel(
@@ -19,3 +24,16 @@ def handle_command(cmd: int, args: int | None = None):
     )
 
     lora_send(data)
+
+
+# this is the plan rn
+# Ground station sends request including number
+# PSAT parsed command if it recieves it
+#   PSAT extracts sequence number
+#   PSAT checks if sequence number is more than last stored sequence number
+#       if so: sends back an ACK and does not run code
+#       if not: runs command or request and then sends ack
+# after ground station sends command
+# it does a block in the write loop that checks if most recent ack is bigger than most recent sequence number in the queue
+#   if it is: item is finally popped from the queue
+#   if not: item is popped from the queue, ground station sends same request with same number, and then ads back to the queue
